6595606f290375613788a41446abadc0223e874b
DevOpsMBAConsultings <devops@mbaconsultings.com>
2026-06-17 16:59:52 -0500

    fix: Agregar campo id a la vista de formulario para arreglar visibilidad del botón de descargar (Fase 6)

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Gestión de Reparaciones de Flota',
-    'version': '16.0.1.0.21',
+    'version': '16.0.1.0.22',
     'category': 'Human Resources/Fleet',
     'summary': 'Control de inventario para reparaciones y mantenimientos de vehículos',
     'author': 'MBA Consultings',
